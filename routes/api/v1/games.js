6de6014b48d5ff136aa10bd83931b2b4245cae91
Trevor Nodland <trevor.nodland@gmail.com>
2019-08-10 08:50:28 -0600

    completes arcade workshop

---
+++ routes/api/v1/games.js
@@ -0,0 +1,90 @@
+var express = require("express");
+var router = express.Router();
+var Game = require('../../../models').Game;
+
+router.get("/", function(req, res, next) {
+  Game.findAll()
+    .then(games => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(200).send(JSON.stringify(games));
+    })
+    .catch(error => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(500).send({error})
+    });
+});
+
+router.get('/:id', function(req, res, next) {
+  Game.findAll({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(game => {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).send(JSON.stringify(game));
+  })
+  .catch(error => {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(500).send({error});
+  });
+});  
+
+
+router.post("/", function(req, res, next) {
+  Game.create({
+          title: req.body.title,
+          price: req.body.price,
+          releaseYear: req.body.releaseYear,
+          active: req.body.active
+    })
+    .then(game => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(201).send(JSON.stringify(game));
+    })
+    .catch(error => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(500).send({ error });
+    });
+});
+
+router.put('/:id', function(req, res, next) {
+  Game.update({
+    title: req.body.title,
+    price: req.body.price,
+    releaseYear: req.body.releaseYear,
+    active: req.body.active
+  },
+  {
+    returning: true,
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(game => {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).send(JSON.stringify(game));
+  })
+  .catch(error => {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(500).send({error});
+  });
+});
+
+router.delete('/:id', function(req, res, next) {
+  Game.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(game => {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(204);
+  })
+  .catch(error => {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(500).send({error});
+  });
+});
+
+module.exports = router;
